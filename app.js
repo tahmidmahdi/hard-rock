a3a44ad10e5ddaf41b000fbe1d2a22a8bf5ef9fd
tahmidmahdi <tahmid5550@gmail.com>
2021-02-09 01:26:37 +0600

    songs preview, artist, lyrics also clearing previous results

--- app.js
+++ app.js
@@ -10,19 +10,35 @@ const searchSongs = () => {
 }
 const displaySongs = songs => {
     const songContainer = document.getElementById('song-container');
+    songContainer.innerHTML = '';
     songs.forEach(song => {
-        console.log(song)
+        // console.log(song)
         const songDiv = document.createElement('div');
         songDiv.className = 'single-result row align-items-center my-3 p-3';
         songDiv.innerHTML = ` <div class="col-md-9">
         <h3 class="lyrics-name">${song.title}</h3>
         <p class="author lead">Album by <span>${song.artist.name}</span></p>
+        <audio controls>
+        <source src="${song.preview}" type="audio/mpeg">
+        </audio>
+
         </div>
         <div class="col-md-3 text-md-right text-center">
-            <button class="btn btn-success">Get Lyrics</button>
+            <button onClick="getLyric('${song.artist.name}','${song.title}' )" class="btn btn-success">Get Lyrics</button>
         </div>`;
         songContainer.appendChild(songDiv);
 
     });
+}
+const getLyric = (artist, title) =>{
+    const url = ` https://api.lyrics.ovh/v1/${artist}/${title}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayLyrics(data.lyrics))
+}
+
+const displayLyrics = lyrics =>{
+    const lyricsDiv = document.getElementById('song-lyrics');
+    lyricsDiv.innerText = lyrics;
 }
 
