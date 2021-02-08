105f2e689fbf1ff2a507948276d237943bdf0500
tahmidmahdi <tahmid5550@gmail.com>
2021-02-09 00:38:49 +0600

    songs and artist names have been showed

---
+++ app.js
@@ -0,0 +1,28 @@
+const searchSongs = () => {
+    const searchText = document.getElementById('search-field').value;
+    const url = `https://api.lyrics.ovh/suggest/:${searchText}`
+    //load data
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displaySongs(data.data))
+
+
+}
+const displaySongs = songs => {
+    const songContainer = document.getElementById('song-container');
+    songs.forEach(song => {
+        console.log(song)
+        const songDiv = document.createElement('div');
+        songDiv.className = 'single-result row align-items-center my-3 p-3';
+        songDiv.innerHTML = ` <div class="col-md-9">
+        <h3 class="lyrics-name">${song.title}</h3>
+        <p class="author lead">Album by <span>${song.artist.name}</span></p>
+        </div>
+        <div class="col-md-3 text-md-right text-center">
+            <button class="btn btn-success">Get Lyrics</button>
+        </div>`;
+        songContainer.appendChild(songDiv);
+
+    });
+}
+
